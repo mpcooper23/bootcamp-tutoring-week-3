afb6c37ce30eaca6fe8e1d7a1b3f1e1450c5b393
Alex Aaron <alexfrancisaaron@gmail.com>
2025-01-13 11:57:37 -0600

    removed solution code

--- day-1/recursion.js
+++ day-1/recursion.js
@@ -24,16 +24,6 @@ function getTasksWithTag(array){
 }
 
 // Problem #6 //
-function getObjects(array, output={}){
-    if (array.length === 0){
-        return output;
-    }
-    output[array[0].task] = array[0].recurs + " - Priority: " + array[0].priority;
-    return getObjects(array.slice(1), output);
+function getObjects(array){
+    
 };
-
-console.log(getObjects(tasks));
-
-
-// use recursion to create an object
-
